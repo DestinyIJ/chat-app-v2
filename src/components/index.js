31d441f94cfdc424954ce0b4926fed3a23883a9c
DestinyIJ <ighedodestiny11@gmail.com>
2023-03-18 22:54:28 +0100

    chapter1

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as AntSwitch } from './AntSwitch'
+export { default as Iconify } from './Iconify'
+export { default as LoadingScreen } from './LoadingScreen'
+export { default as Scrollbar } from './Scrollbar'
