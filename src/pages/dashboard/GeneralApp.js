0eafe3da422b8d49f429dae2267c0c58f4bca97c
DestinyIJ <ighedodestiny11@gmail.com>
2023-03-29 21:32:15 +0100

    chapter 3

--- src/pages/dashboard/GeneralApp.js
+++ src/pages/dashboard/GeneralApp.js
@@ -1,11 +1,28 @@
 import React from "react";
 import Chats from "./Chats";
+import { Stack, Box } from '@mui/material'
+import { useTheme } from "@mui/material/styles"
+import { ChatBox } from "../../components";
 
 const GeneralApp = () => {
+  const theme = useTheme()
 
   return (
     <>
-      <Chats />
+      <Stack direction={'row'} sx={{ width: "100%"}}>
+        {/* chat list */}
+        <Chats />
+        {/* chat box */}
+        <Box 
+          sx={{
+            height: "100%",
+            width: "100%",
+            backgroundColor: theme.palette.mode === "light" ? "#FFF" : theme.palette.background.default,
+          }}
+        >
+          <ChatBox />
+        </Box>
+      </Stack>
     </>
   );
 };
