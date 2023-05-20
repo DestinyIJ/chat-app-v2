03de66967405b3e7d09bc6ac160019e469a66e48
DestinyIJ <ighedodestiny11@gmail.com>
2023-05-20 14:34:07 +0100

    change folder structure

--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-export { default as AntSwitch } from './AntSwitch'
-export { default as Iconify } from './Iconify'
-export { default as LoadingScreen } from './LoadingScreen'
-export { default as Scrollbar } from './Scrollbar'
-export { default as ChatBox } from './chatbox/ChatBox'
-export { default as StyledBadge } from './StyledBadge'
